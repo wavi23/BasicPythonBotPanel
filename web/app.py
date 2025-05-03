9aacd34696d55f438549877264ab0bbba26fb6fd
wavi23 <wiktoreq12@op.pl>
2025-05-03 22:09:13 +0200

    Added navbar

--- web/app.py
+++ web/app.py
@@ -5,10 +5,13 @@ app = Flask(__name__)
 # Load configuration from config.py
 app.config.from_pyfile('config.py')
 title = app.config['TITLE']
+nav_logo = app.config['NAV_LOGO']
+nav_title = app.config['NAV_TITLE']
+nav_discord_url = app.config['NAV_DISCORD_URL']
 
 @app.route('/')
 def index():
-	return render_template('index.html', title=title)
+	return render_template('index.html', title=title, nav_logo=nav_logo, nav_title=nav_title, nav_discord_url=nav_discord_url)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- web/config.py
+++ web/config.py
@@ -1,1 +1,4 @@
 TITLE = "A simple Bot Panel"
+NAV_LOGO = "" 
+NAV_TITLE = "A simple Bot Panel"
+NAV_DISCORD_URL = "https://discord.gg/your-discord-link"
