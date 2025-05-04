a0cab5374a3e87958bc1b7ef57e0371c8058c642
wavi23 <wiktoreq12@op.pl>
2025-05-04 21:06:15 +0200

    Update config.py

--- web/config.py
+++ web/config.py
@@ -2,3 +2,15 @@ TITLE = "A simple Bot Panel"
 NAV_LOGO = "https://cdn.discordapp.com/attachments/1320126454662434886/1332145450475851836/31a656ec4058b979b9d3639815fb812e.png?ex=6818af61&is=68175de1&hm=723593eb3f32c0b56e29a4ece75195beadec4168a127c4e199f2f0e61a42503a&" 
 NAV_TITLE = "Bot Panel"
 NAV_DISCORD_URL = "https://discord.gg/your-discord-link"
+
+CARD1_TITLE = "6+"
+CARD1_DESC = "Total Servers. We are growing fast!"
+
+CARD2_TITLE = "6+"
+CARD2_DESC = "Total Servers. We are growing fast!"
+
+CARD3_TITLE = "6+"
+CARD3_DESC = "Total Servers. We are growing fast!"
+
+CARD4_TITLE = "6+"
+CARD4_DESC = "Total Servers. We are growing fast!"
