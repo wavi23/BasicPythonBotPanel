b4b3a11358fc4eb86778aefc849502eaa6a67c35
wavi23 <wiktoreq12@op.pl>
2025-05-04 15:33:01 +0200

    Added logo and styles to logo in navbar

--- web/config.py
+++ web/config.py
@@ -1,4 +1,4 @@
 TITLE = "A simple Bot Panel"
-NAV_LOGO = "" 
-NAV_TITLE = "A simple Bot Panel"
+NAV_LOGO = "https://cdn.discordapp.com/attachments/1320126454662434886/1332145450475851836/31a656ec4058b979b9d3639815fb812e.png?ex=6818af61&is=68175de1&hm=723593eb3f32c0b56e29a4ece75195beadec4168a127c4e199f2f0e61a42503a&" 
+NAV_TITLE = "Bot Panel"
 NAV_DISCORD_URL = "https://discord.gg/your-discord-link"
